055d1d9bd105332b2f658b413afd771a54687456
GoshaGG21 <72431085+GoshaGG21@users.noreply.github.com>
2024-02-19 17:01:21 +0400

    Update MyProject2.UE4.js

--- MyProject2.UE4.js
+++ MyProject2.UE4.js
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('UE4Game.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/GoshaGG21/-/b6c6bd8aa411bf40c9aa080afb3b39ee1a5f4579/UE4Game.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
